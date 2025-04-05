0f3b7bf764fd8fa69ac46022f16ab38ff1ec577c
toni1423 <todorow.1423@gmail.com>
2025-04-05 16:00:09 +0300

    Update CalorieResponse.java

--- calorieCalculator-svc/src/main/java/com/app/caloriecalculatorsvc/dto/CalorieResponse.java
+++ calorieCalculator-svc/src/main/java/com/app/caloriecalculatorsvc/dto/CalorieResponse.java
@@ -1,11 +1,37 @@
-
 package com.app.caloriecalculatorsvc.dto;
 
-public class CalorieResponse {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+public class CalorieCalculation {
+    @NotNull
+    private UUID userId;
+
+    @NotNull
+    private double weight;
+
+    @NotNull
+    private double height;
+
+    @NotNull
+    private int age;
 
     private double calories;
 
-    public CalorieResponse(double calories) {
-        this.calories = calories;
-    }
+    @NotNull
+    private String gender;
+
+    @NotNull
+    @NotBlank
+    private String activityLevel;
+
+    private LocalDateTime calculatedAt;
+
 }
